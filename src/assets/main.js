4725f730b997cc9b00c722edd3cd078abb1f6c36
Unai Recio <unai.recio@agcpartners.co.uk>
2016-12-15 15:35:18 +0100

    feat(main): Fetch courses and print them

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/srcrustaceo.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      response.courses.completed.forEach(function (courseCompleted) {
+        var courseContent = $('<div class="course">')
+        .append('<h3>'+courseCompleted.title+'</h3>')
+        .append('<img src="'+courseCompleted.badge+'">')
+        .append('<a href="'+courseCompleted.url+'" target="_blank" class="btn btn-primary">See Course</a>');
+
+        $('#badges')
+        .append(courseContent);
+
+      });
+    }
+  });
+
 
 });
